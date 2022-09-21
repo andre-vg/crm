b4191cf41876c485521d76bea168c8e2fcfcf74f
andre-vg <avictorg@gmail.com>
2022-09-21 13:28:42 -0300

    ♻️ mudança de planos

--- src/App.js
+++ src/App.js
@@ -2,58 +2,24 @@ import "./index.css";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import Card from "./componentes/Card";
-import { Route, Router, Routes, BrowserRouter } from "react-router-dom";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import Nav from "./componentes/Navbar";
+import Camp from "./pages/Camp";
 
 function App() {
   return (
-    <div className="App">
-      <nav class="navbar navbar-expand-lg navbar-dark bg-[#316181]">
-        <div class="container-fluid">
-          <a class="font-bold text-[#316181] h-auto" href="#">
-            <img
-              className="max-h-12 my-1"
-              src="http://portal.rybena.com.br/site-rybena/assets/img/logos/ryb_logo_big.png"
-            />
-          </a>
-          <button
-            class="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNavAltMarkup"
-            aria-controls="navbarNavAltMarkup"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-          >
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav">
-              <a class="nav-link ml-4" aria-current="page" href="/">
-                Home
-              </a>
-              <a class="nav-link" aria-current="page" href="/jogadores">
-                Jogadores
-              </a>
-              <a class="nav-link" aria-current="page" href="/pontos">
-                Pontos
-              </a>
-            </div>
-          </div>
-        </div>
-      </nav>
-
-      <div className="w-full text-center font-sans">
-        <h1 className="text-5xl font-bold">
-          Copa <strong className="text-[#316181]">Rybená</strong> de Magic (CRM)
-        </h1>
+    <>
+      <div className="absolute h-full w-full top-0 left-0 bg-blue-gray-50 -z-50 bg-cover"></div>
+      <div>
+        <Nav />
+        <BrowserRouter>
+          <Routes>
+            <Route path="/jogadores" element={<Card />} />
+            <Route path="/campeonatos" element={<Camp />} />
+          </Routes>
+        </BrowserRouter>
       </div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/jogadores" element={<Card />} />
-          {/* <Route path="/pontos" element={<Card />} /> */}
-        </Routes>
-      </BrowserRouter>
-    </div>
+    </>
   );
 }
 
